4686e2e828575fea333fd24b9c1f80919e9cae7e
Fer-efe <ferpolanco83@gmail.com>
2024-07-16 10:44:43 -0300

    Fix: contenedores

--- VisibilidadDivs/script.js
+++ VisibilidadDivs/script.js
@@ -1,7 +1,7 @@
 function toggleDivs() {
-    const div1 = document.getElementById('div1');
-    const div2 = document.getElementById('div2');
+    const div1 = document.getElementById("div1");
+    const div2 = document.getElementById("div2");
     
-    div1.classList.toggle('hidden');
-    div2.classList.toggle('hidden');
+    div1.classList.toggle("hidden");
+    div2.classList.toggle("hidden");
 }
