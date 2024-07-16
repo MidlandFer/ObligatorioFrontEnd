cd2df14d575f47fb4f8c893902d8e6bd2aa530b4
Fer-efe <ferpolanco83@gmail.com>
2024-07-16 10:04:43 -0300

    New: Agrego los archivos del ejercicio

---
+++ VisibilidadDivs/script.js
@@ -0,0 +1,7 @@
+function toggleDivs() {
+    const div1 = document.getElementById('div1');
+    const div2 = document.getElementById('div2');
+    
+    div1.classList.toggle('oculto');
+    div2.classList.toggle('oculto');
+}
