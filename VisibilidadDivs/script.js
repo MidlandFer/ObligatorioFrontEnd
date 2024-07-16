ccd26a0a5e02dc152f4ad31f79911d79a5f00326
Fer-efe <ferpolanco83@gmail.com>
2024-07-16 10:26:50 -0300

    Fix: agrego las funcionalidadeds

--- VisibilidadDivs/script.js
+++ VisibilidadDivs/script.js
@@ -2,6 +2,6 @@ function toggleDivs() {
     const div1 = document.getElementById('div1');
     const div2 = document.getElementById('div2');
     
-    div1.classList.toggle('oculto');
-    div2.classList.toggle('oculto');
+    div1.classList.toggle('hidden');
+    div2.classList.toggle('hidden');
 }
